5cb876bec9b0df8036bbf05e7077570482747c22
cleo <cleo@test.ru>
2023-01-30 16:33:46 +0700

    add docker secret

--- backend/project/manage.py
+++ backend/project/manage.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
+from utils.get_secret import get_secret
 
 if __name__ == "__main__":
-    if not os.environ.get("DJANGO_SETTINGS_MODULE", None):
+    module = get_secret("DJANGO_SETTINGS_MODULE", None)
+    if module:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", module)
+    else:
         # для случая запуска локально без докера
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.dev")
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
