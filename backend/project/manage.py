b2c80bc7acfd76c7f7b9eb0d12dcd5e3fb20459e
cleo <cleo@test.ru>
2023-01-30 17:11:32 +0700

    remove secret

--- backend/project/manage.py
+++ backend/project/manage.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
-from utils.get_secret import get_secret
 
 if __name__ == "__main__":
-    module = get_secret("DJANGO_SETTINGS_MODULE", None)
-    if module:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", module)
-    else:
+    if not os.environ.get("DJANGO_SETTINGS_MODULE", None):
         # для случая запуска локально без докера
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.dev")
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
